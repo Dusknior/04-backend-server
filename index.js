2c13701401e72534ca7f9f508581be29076f5cee
Daniel Bernal Herrera <danielbernalherrera@gmail.com>
2022-04-26 08:18:40 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const { dbConnection } = require('./database/config');
+
+//Crear el servidor de express
+const app = express();
+
+//Base de datos
+dbConnection();
+
+//Configurar CORS
+app.use(cors());
+
+//Rutas
+app.get('/', (req, res) => {
+    res.json({ok: true, msg: 'Hola mundo'});
+})
+
+app.listen(process.env.PORT, () => {
+    console.log('Server on port', process.env.PORT);
+})
